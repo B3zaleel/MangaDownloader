31b87fc7594f59695d125e0d6e43eb30c180c527
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 18:36:20 +0100

    Add manga retriever interface

---
+++ MangaRetrievers/IMangaRetriever.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MangaDownloader.MangaRetrievers
+{
+    public interface IMangaRetriever
+    {
+        string RetrieverName { get; }
+        string HomePage { get; }
+        void FetchManga(List<Manga> mangas, string mangaURL);
+        Chapter FetchChapter(Manga parent, string chapterURL);
+        void UpdateChapters(Manga parent);
+    }
+}
