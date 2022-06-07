bbd1c49ac8fac785bf1f42c56835c94731feeb4e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 18:13:00 +0100

    Add converter for boolean state switching

---
+++ Converters/BooleanStateSwitchingConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MangaDownloader.Converters
+{
+    public class BooleanStateSwitchingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((bool)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((bool)value);
+        }
+    }
+}
