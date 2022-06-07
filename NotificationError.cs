f6e4924e9ee10edcfbc013acfbe420ce437976b2
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 18:04:44 +0100

    Add NotificationError class

---
+++ NotificationError.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MangaDownloader
+{
+    public enum NotificationType
+    {
+        None,
+        FinishedTask,
+        Success,
+        Error,
+    }
+
+    public class NotificationError : Exception
+    {
+        public string Notification { get; private set; }
+        public NotificationType Type { get; private set; }
+
+        public NotificationError(string notificationMsg, NotificationType notificationType = NotificationType.None)
+        {
+            Notification = notificationMsg;
+            Type = notificationType;
+        }
+    }
+}
