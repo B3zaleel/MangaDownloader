19cdab1ad9274bacecaa7d82dfbc93d19049076a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 18:02:52 +0100

    Add Page class

---
+++ Page.cs
@@ -0,0 +1,14 @@
+﻿namespace MangaDownloader
+{
+    public class Page
+    {
+        public Page(Chapter parent)
+        {
+            Parent = parent;
+        }
+
+        public bool Saved { get; set; }
+        public string Address { get; set; }
+        public Chapter Parent { get; }
+    }
+}
