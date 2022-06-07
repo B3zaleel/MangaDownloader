0cf6a423eea947e558af94d16e027d33fed183a5
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 19:14:13 +0100

    Add manga downloader settings class

---
+++ IOUtilities/MangaDownloaderSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MangaDownloader.IOUtilities
+{
+    public enum SerializationType
+    {
+        JSON,
+        JSONMinified,
+        MDBin,
+    }
+
+    public class MangaDownloaderSettings
+    {
+        public static void Serialize(string path, List<Manga> mangas, SerializationType serializationType)
+        {
+            switch (serializationType)
+            {
+                case SerializationType.JSON:
+                    JSONFormat.Serialize(path, mangas, false);
+                    break;
+                case SerializationType.JSONMinified:
+                    JSONFormat.Serialize(path, mangas, true);
+                    break;
+                case SerializationType.MDBin:
+                    MDBinFormat.Serialize(path, mangas);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unimplemeted Serialization Type");
+            }
+        }
+
+        public static List<Manga> Deserialize(string path, SerializationType serializationType)
+        {
+            switch (serializationType)
+            {
+                case SerializationType.JSON:
+                case SerializationType.JSONMinified:
+                    return JSONFormat.Deserialize(path);
+                case SerializationType.MDBin:
+                    return MDBinFormat.Deserialize(path);
+                default:
+                    throw new InvalidOperationException("Unimplemeted Serialization Type");
+            }
+        }
+    }
+}
