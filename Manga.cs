cbebf5bf32f31dcb934174145fb7555b165918cc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 18:04:10 +0100

    Add Manga class

---
+++ Manga.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace MangaDownloader
+{
+    public enum BookFormats
+    {
+        none,
+        cbz,
+        cb7,
+    }
+
+    public enum ImageTypes
+    {
+        None,
+        BMP,
+        GIF,
+        JPEG,
+        JPG,
+        PNG,
+    }
+
+    public class Manga : INotifyPropertyChanged
+    {
+        public Manga()
+        {
+            IsAvailable = true;
+            OtherProps = new Dictionary<string, string>();
+        }
+
+        public string Title { get; set; }
+        public BookFormats BookFormat { get; set; }
+        public string RetrieverName { get; set; }
+        public CoverImage Cover
+        {
+            get { return cover; }
+            set
+            {
+                cover = value;
+                OnPropertyChanged("Cover");
+            }
+        }
+        public Dictionary<string, string> OtherProps { get; set; }
+        public List<Chapter> Chapters { get; set; }
+        public string Description { get; set; }
+        public int ChaptersCount
+        {
+            get { return chaptersCount; }
+            set
+            {
+                chaptersCount = value;
+                OnPropertyChanged("ChaptersCount");
+            }
+        }
+        public string[] Genres { get; set; }
+        public string Address { get; set; }
+
+
+        public bool IsAvailable { get; set; }
+        public bool IsUpdatingChapters { get; set; }
+
+        private CoverImage cover;
+        private int chaptersCount;
+
+        public void MergeChapters(List<Chapter> chapters)
+        {
+            Chapters.AddRange(chapters);
+            Chapter.SortChapters(Chapters);
+            Chapters.Reverse();
+            ChaptersCount += chapters.Count;
+            OnPropertyChanged("Chapters");
+        }
+
+        public void MergeChapter(Chapter chapter)
+        {
+            Chapters.Add(chapter);
+            Chapter.SortChapters(Chapters);
+            Chapters.Reverse();
+            ChaptersCount++;
+            OnPropertyChanged("Chapters");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
