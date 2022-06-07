cc6c4abe0f00513c85ae58331b9c639b710a4774
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-07 18:11:32 +0100

    Add converters for chapter list items

---
+++ Converters/ChapterListItemConverters.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MangaDownloader.Converters
+{
+    public class ProgressToMarginConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((byte)value == 100)
+                return new Thickness(10, 0, 10, 0);
+            else
+                return new Thickness(7.5, 0, 7.5, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Thickness)value).Left == 10 && ((Thickness)value).Right == 10 ? 100 : 0;
+        }
+    }
+
+    public class ProgressToCornerRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((byte)value == 100)
+                return 15;
+            else
+                return 5;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (byte)value == 5 ? 100 : 0;
+        }
+    }
+
+    public class ProgressToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((byte)value == 100)
+                return new SolidColorBrush(Colors.LightSeaGreen);
+            else
+                return new SolidColorBrush(Colors.DarkGray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((SolidColorBrush)value).Color == Colors.LightSeaGreen ? 100 : 0;
+        }
+    }
+
+    public class ProgressToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((byte)value == 100)
+                return "";
+            else
+                return $"{value}%";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString().Length == 0 ? 100 : byte.Parse(value.ToString().TrimEnd('%'));
+        }
+    }
+
+    public class ProgressToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((byte)value == 100)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility.Visible;
+        }
+    }
+
+    public class CompletionToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility.Visible;
+        }
+    }
+
+    public class IsUpdateEnabledToUpdateIconVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is Visibility.Visible;
+        }
+    }
+
+    public class IsUpdateEnabledToUpdateLoadingVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(bool)value)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(value is Visibility.Visible);
+        }
+    }
+}
