7de17b52289f4545625effc4fc4550147474b4c4
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-07-05 20:59:57 +0100

    Create NotificationEventArgs for notification events

---
+++ Events/NotificationEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MangaDownloader.Events
+{
+    /// <summary>
+    /// Represents the method that will handle the NotificationEventArgs event raised when an operation's status has changed.
+    /// </summary>
+    /// <param name="sender">The source of the event.</param>
+    /// <param name="e">A NotificationEventArgs that contains the event data.</param>
+    public delegate void NotificationRaisedEventHandler(object sender, NotificationEventArgs e);
+
+    /// <summary>
+    /// Contains state information and event data associated with a notification event.
+    /// </summary>
+    public class NotificationEventArgs : RoutedEventArgs
+    {
+        public NotificationEventArgs(string message, NotificationType type)
+        {
+            this.Message = message;
+            this.NotificationType = type;
+        }
+
+        public string Message { get; private set; }
+        public NotificationType NotificationType { get; private set; }
+    }
+}
